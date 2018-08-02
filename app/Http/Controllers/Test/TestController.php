bc1d2fb6407c8b8aa4296eee35dad62ba54a16f4
Huisan <1520948986@qq.com>
2018-08-02 10:46:21 +0800

    update testcontroller.php

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -14,7 +14,7 @@ class TestController extends Controller
 {
     public function index()
     {
-        return 'this Test---index';
+        return 'this Test- - -index';
     }
 
     public function insert()
